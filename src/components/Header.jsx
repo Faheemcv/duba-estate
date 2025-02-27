cee9ca2920577b8d417c1453d3730e29a406c115
Faheemcv <faheemmohammed703@gmail.com>
2025-02-27 22:49:15 +0530

    complete sign up page fuctionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     // logo
     <header className='bg-slate-200 shadow-md'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3 '>
           <Link to='/'>
             <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
                 <span className='text-slate-500'>DUBA</span>
