858f0d8221b17de2e7362594ca6d88fd0995f5e1
Faheemcv <faheemmohammed703@gmail.com>
2025-03-30 12:57:49 +0530

    create get user listings api route

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,6 @@ import userReducer from './user/userSlice';
 import { persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage';
 
-
 const rootReducer = combineReducers({user: userReducer})
 
 const persistConfig = {
@@ -13,7 +12,6 @@ const persistConfig = {
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
-
 
 export const store = configureStore({
   reducer: persistedReducer,
