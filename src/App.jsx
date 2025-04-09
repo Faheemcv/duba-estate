f41187fbb4424c826d3d9a7a8a91cc5be64e98d4
Faheemcv <faheemmohammed703@gmail.com>
2025-04-09 13:03:49 +0530

    add image slide to the listing page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Header from "./components/Header"
 import PriveteRoute from "./components/PriveteRoute"
 import CreateListing from "./pages/CreateListing"
 import UpdateListing from "./pages/UpdateListing"
+import Listing from "./pages/Listing"
 
 export default function App() {
   return (
@@ -17,7 +18,10 @@ export default function App() {
           <Route path="/" element={<Home />} />        
           <Route path="/sign-in" element={<SignIn />} />  
           <Route path="/sign-up" element={<SingUp />} />  
-          <Route path="/about" element={<About />} />  
+          <Route path="/about" element={<About />} />
+          
+          <Route path='/listing/:listingId' element={<Listing />} />
+    
           <Route element={<PriveteRoute />} >
             <Route path="/profile" element={<Profile />} /> 
             <Route path="/create-listing" element={<CreateListing />} /> 
